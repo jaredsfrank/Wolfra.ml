4ebed6031b51bc54e6e8a6e8cd18774791b2c86d
Jared Frank <jaredsfrank@gmail.com>
2015-11-22 23:04:30 -0500

    Finished swapping variants to simplification friendly functions

--- eval.ml
+++ eval.ml
@@ -170,15 +170,15 @@ let rec deriv s1 s2 =
   | SSin x, SVar x' -> (match x with
               | SFloat v -> SFloat 0.
               | SVar v -> if (v = x') then (SCos x) else (SFloat 0.)
-              | _ -> (times ((SCos x),(deriv x s2)) )
+              | _ -> (times ((SCos x),(deriv x s2)) ))
   | SCos x, SVar x' -> (match x with
               | SFloat x -> SFloat 0.
               | SVar v -> if (v = x') then times ((SFloat (-1.)),(SSin x)) else SFloat 0.
-              | _ -> times ((SFloat (-1.)), times ((SSin x);(deriv x s2)))
+              | _ -> times ((SFloat (-1.)), times ((SSin x),(deriv x s2))))
   | SLog x, SVar x' -> (match x with
                    | SFloat v -> SFloat 0.
                    | SVar v -> if (v=x') then (pow (x, SFloat (-1.))) else SFloat 0.
-                   | _ -> times ((pow (x, SFloat (-1.))), deriv x s2)
+                   | _ -> times ((pow (x, SFloat (-1.))), deriv x s2))
 
   | SPI, SVar x' -> SFloat 0.
   | SE, SVar x' -> SFloat 0.
