879676d54769b81fa49eadca32fefa7eedce2d69
Jared Frank <jaredsfrank@gmail.com>
2015-11-22 21:59:03 -0500

    Added a catch all for main.js

--- main.ml
+++ main.ml
@@ -14,5 +14,6 @@ let rec main () =
     | Failure "TODO" -> Printf.printf "That feature is not yet implemented\n\n"; main()
     | Failure "Division by 0" -> Printf.printf "Uh oh...Division by 0\n\n"; main()
     | Failure "This shouldn't happen" -> Printf.printf "Please derive with respect to a variable\n\n"; main()
+    | _  -> Printf.printf "Please enter in a proper expression\n\n"; main()
 
 let () = main ()
