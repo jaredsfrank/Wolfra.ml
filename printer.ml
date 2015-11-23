43eff0e9c5daed52157cf941d6b2b2518c574d79
Jared Frank <jaredsfrank@gmail.com>
2015-11-22 20:36:31 -0500

    Fixed parentheses for printing

--- printer.ml
+++ printer.ml
@@ -6,15 +6,15 @@ let rec format_expr f e =
         Format.fprintf f "%a" format_expr e
     in
     match e with
-    | SFloat n -> Format.fprintf f "%.2f" n
-    | SVar x -> Format.fprintf f "%s" x
-    | STimes [] -> Format.fprintf f ""
+    | SFloat n -> Format.fprintf f "@[%.2f@]" n
+    | SVar x -> Format.fprintf f "@[%s@]" x
+    | STimes [] -> Format.fprintf f "@[@]"
     | STimes [h] -> Format.fprintf f "@[%a@]" (bracket e) h
     | STimes (h::t) -> Format.fprintf f "@[%a*%a@]" (bracket e) h (bracket e) (STimes t)
-    | SPlus [] -> Format.fprintf f ""
+    | SPlus [] -> Format.fprintf f "@[@]"
     | SPlus [h] -> Format.fprintf f "@[%a@]" (bracket e) h
     | SPlus (h::t) -> Format.fprintf f "@[%a+%a@]" (bracket e) h (bracket e) (SPlus t)
-    | SPow (s1,s2) -> Format.fprintf f "@[%a^(%a)@]" (bracket e) s1 (bracket e) s2
+    | SPow (s1,s2) -> Format.fprintf f "@[(%a)^(%a)@]" (bracket e) s1 (bracket e) s2
     | SMatrix x ->  failwith "TODO"
     | SSin s ->  Format.fprintf f "@[sin(%a)@]" (bracket e) s
     | SCos s ->  Format.fprintf f "@[cos(%a)@]" (bracket e) s
