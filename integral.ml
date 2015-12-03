a10af2edc8aac2ed6db7e114a6861f1b5cd1db5d
camilleamiri <camilleamiri@yahoo.com>
2015-12-02 21:41:39 -0500

    Update integral.ml

--- integral.ml
+++ integral.ml
@@ -1,23 +1,7 @@
 open Simplify
 open Derivative
 
-let rec is_constant x' = function
-    | SFloat f           -> true
-    | SVar x when x = x' -> false
-    | SVar x             -> true
-    | STimes (c, l)      -> List.fold_left
-                            (fun accum a -> accum && is_constant x' a) true l
-    | SPlus l            -> List.fold_left
-                            (fun accum a -> accum && is_constant x' a) true l
-    | SPow (e1, e2)      -> is_constant x' e1 && is_constant x' e2
-    | SMatrix l          -> List.fold_left (fun accum l' -> accum &&
-                            List.fold_left (fun accum a -> accum &&
-                            is_constant x' a) true l') true l
-    | SSin e             -> is_constant x' e
-    | SCos e             -> is_constant x' e
-    | SLog e             -> is_constant x' e
-    | SE                 -> true
-    | SPI                -> true
+let is_constant s = true (*JUST A PLACEHOLDER. STILL NEEDS TO BE DONE*)
 
 let rec integrate s1 s2 =
  match s1, s2 with
@@ -31,7 +15,7 @@ let rec integrate s1 s2 =
  | SPlus [h], SVar _     -> integrate h s2
  | SPlus (h::t), SVar _  -> s_plus [integrate h s2; integrate (SPlus t) s2]
  | SPow (SVar x, SFloat (-1.)), SVar x' when x = x' -> SLog(SVar x)
- | SPow (SVar x, g), SVar x' when x = x' && is_constant x' g ->  times(pow(SVar x, plus(SFloat 1., g)),pow(plus(SFloat 1., g), SFloat (-1.)))
+ | SPow (SVar x, g), SVar x' when x = x' && is_constant g ->  times(pow(SVar x, plus(SFloat 1., g)),pow(plus(SFloat 1., g), SFloat (-1.)))
  | SPow (SE, SVar x), SVar x' when x = x' -> pow(SE, SVar x)
  | SPow (SE, STimes(c,[SVar x])), SVar x' when x = x' ->times(SFloat (1./.c), SPow(SE, STimes(c,[SVar x])))
  | SPow (SVar f, SFloat g), SVar x' when f = x' -> times(pow(plus(SFloat g, SFloat 1.), SFloat (-1.)),pow(SVar f,plus(SFloat g, SFloat 1.)))
@@ -46,6 +30,8 @@ let rec integrate s1 s2 =
                             | SVar v when v <> x'-> times(SSin x, SVar x')
                             | STimes(f, [SVar v]) when v=x' -> times(pow(times(SFloat (-1.), SFloat f), SFloat (-1.)), SCos(x))
                             | STimes(f, [SVar v]) when v<>x' -> times(SSin x, SVar x')
+                            | STimes(f, SVar v1::[SVar v2]) when v1=x' -> times(pow(s_times(SFloat f::[SVar v2]),SFloat (-1.)),times(SFloat (-1.),SCos x))
+                            | STimes(f, SVar v1::[SVar v2]) when v2=x' -> times(pow(s_times(SFloat f::[SVar v1]),SFloat (-1.)),times(SFloat (-1.),SCos x))
                             | SPlus(SVar v::[SFloat f]) | SPlus(SFloat f::[SVar v]) when v = x'-> times(SFloat (-1.), SCos(SPlus(SVar v::[SFloat f])))
                             | SPlus(SVar v::[SFloat f]) | SPlus(SFloat f::[SVar v]) when v <> x'-> times(SSin x, SVar x')
                             | _ -> failwith "TODO")
@@ -56,6 +42,8 @@ let rec integrate s1 s2 =
                             | SVar v when v <> x' -> times(SCos x, SVar x')
                             | STimes(f, [SVar v]) when v=x' -> times(pow(SFloat f, SFloat (-1.)), SSin(x))
                             | STimes(f, [SVar v]) when v<>x' -> times(SCos x, SVar x')
+                            | STimes(f, SVar v1::[SVar v2]) when v1=x' -> times(pow(s_times(SFloat f::[SVar v2]),SFloat (-1.)),SSin x)
+                            | STimes(f, SVar v1::[SVar v2]) when v2=x' -> times(pow(s_times(SFloat f::[SVar v1]),SFloat (-1.)),SSin x)
                             | SPlus(SVar v::[SFloat f]) | SPlus(SFloat f::[SVar v]) when v = x'-> SSin(SPlus(SVar v::[SFloat f]))
                             | SPlus(SVar v::[SFloat f]) | SPlus(SFloat f::[SVar v]) when v <> x'-> times(SCos x, SVar x')
                             | _ -> failwith "TODO")
