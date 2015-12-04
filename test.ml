4c0cef9c66c7b69a12675b6c0547bffa09dd0bf3
Jared Frank <jaredsfrank@gmail.com>
2015-12-04 18:59:13 -0500

    More tests

--- test.ml
+++ test.ml
@@ -13,11 +13,27 @@ let evaluate s = string_of_expr(eval(parse_expr s))
 let eval_ast s = eval(parse_expr s)
 
 
+(*Times tests*)
+TEST "times Const" = evaluate "5x" = "5x"
+TEST "Basic times" = evaluate "x*y" = "x*y"
+
+(*NOTE:THIS IS ACTUALLY A GREAT WAY TO TEST. 
+TESTING PROPERTIES JUST REQUIRES THAT THE TWO EQUIVALENT EXPRESSIONS EVLAUATE TO THE SAME VALUE*)
+TEST "Times pows" = evaluate "x^(-5)*x^(2)" = evaluate "x^(-3)" 
+TEST "FOLDING" =  evaluate "(z+w)*(x+y)" = evaluate "x*w+ y*w + x*z + y*z"                                                  
+TEST "" = true                                                  
+TEST "" = true
+TEST "" = true
+
+
 
 TEST "Distributive" = 
     match eval_ast "(x+1)^2" with 
     | SPlus l when lists_equal l [STimes(2.,[SVar "x"]);SFloat 1.; SPow(SVar "x",SFloat 2.)] -> true
     | _ -> false
+
+TEST "Distributive 2" = 
+    evaluate "(x+y)*(x+y)*(x+y)*(x+y)" = evaluate "(x+y)^4"
 
 
 TEST "Derivative Simple" = evaluate "derive x^2 wrt x" = "2x"
